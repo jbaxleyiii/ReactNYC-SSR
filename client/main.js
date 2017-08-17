b7c19556d0306f51b7aec582a157c534e13d8792
James Baxley <james@meteor.com>
2017-08-17 00:48:28 -0400

    write logger

--- client/main.js
+++ client/main.js
@@ -8,14 +8,34 @@ import { ApolloProvider } from "react-apollo";
 import HttpLink from "apollo-link-http";
 import InmemoryCache from "apollo-cache-inmemory";
 
+import { ApolloLink } from "apollo-link-core";
+import { print } from "graphql/language/printer";
+
 import { App } from "/imports/app";
+
+const logger = (operation, forward) => {
+  const { operationName, query, variables, context } = operation;
+
+  // create group
+  console.group(operationName);
+  // log operation
+  console.log({ operationName, variables, context, query: print(query) });
+
+  // make request
+  return forward(operation).map(({ data, errors }) => {
+    // log result
+    console.log({ data, errors });
+    console.groupEnd();
+    return { data, errors };
+  });
+};
 
 export const start = () => {
   hydrate(window.__CSS__);
 
   const client = new ApolloClient({
     cache: new InmemoryCache(window.__APOLLO_STATE__),
-    link: new HttpLink({ uri: "/graphql" }),
+    link: ApolloLink.from([logger, new HttpLink({ uri: "/graphql" })]),
   });
 
   const WrappedApp = (
